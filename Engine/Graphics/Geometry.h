bcbd65292a5ac2d0f3d3d9702bb18ab8967aaf51
Arcaticas <theo.grant9@gmail.com>
2021-09-08 02:20:20 -0600

    OpenGL Geometry set up

--- Engine/Graphics/Geometry.h
+++ Engine/Graphics/Geometry.h
@@ -19,13 +19,20 @@ namespace eae6320
 		class Geometry
 		{
 		
+			
+
+#if defined( EAE6320_PLATFORM_D3D )
 			// Geometry Data
 			//--------------
-
 			eae6320::Graphics::cVertexFormat* m_vertexFormat = nullptr;
-
 			// A vertex buffer holds the data for each vertex
 			ID3D11Buffer* m_vertexBuffer = nullptr;
+#elif defined( EAE6320_PLATFORM_GL )
+			// A vertex buffer holds the data for each vertex
+			GLuint m_vertexBufferId = 0;
+			// A vertex array encapsulates the vertex data as well as the vertex input layout
+			GLuint m_vertexArrayId = 0;
+#endif
 
 		public:
 			eae6320::cResult InitializeGeometry();
