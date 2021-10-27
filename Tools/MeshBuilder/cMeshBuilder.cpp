ff27bef1032ec2c5e1ff1c3baaaa284d03a794c8
Arcaticas <theo.grant9@gmail.com>
2021-10-27 01:30:31 -0600

    first commit

--- Tools/MeshBuilder/cMeshBuilder.cpp
+++ Tools/MeshBuilder/cMeshBuilder.cpp
@@ -1,10 +1,13 @@
 #include "cMeshBuilder.h"
 #include "Engine/Platform/Platform.h"
 #include <Tools/AssetBuildLibrary/Functions.h>
+#include <Engine/Graphics/cMeshParser.h>
 
 eae6320::cResult eae6320::Assets::cMeshBuilder::Build(const std::vector<std::string>& i_arguments)
 {
-	
+	eae6320::Graphics::s_meshData* data = new eae6320::Graphics::s_meshData;
+	eae6320::Graphics::Parser::ReadMeshValues(this->m_path_source, data);
+
 	return eae6320::Platform::CopyFileW(this->m_path_source, this->m_path_target);
 	
 }
